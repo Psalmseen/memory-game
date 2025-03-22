00173af81204f8747a8e487ed43c40abe61f195e
psalmseen <samsonoyebamiji02@gmail.com>
2025-03-22 20:13:48 +0100

    Fix: Shuffle the cards when game is been reset

--- app/hooks/useGameMetrics.ts
+++ app/hooks/useGameMetrics.ts
@@ -1,36 +1,3 @@
-// import { useState, useEffect } from "react";
-
-// const useGameMetrics = () => {
-//   const [bestScore, setBestScore] = useState<number>(Infinity);
-
-//   // Initialize bestScore from localStorage on client side
-//   useEffect(() => {
-//     const savedBestScore = localStorage.getItem("bestScore");
-//     if (savedBestScore) {
-//       setBestScore(parseInt(savedBestScore, 10));
-//     }
-//   }, []);
-
-//   // Update localStorage whenever the best score changes
-//   useEffect(() => {
-//     localStorage.setItem("bestScore", bestScore.toString());
-//   }, [bestScore]);
-
-//   // Function to update the best score
-//   const updateBestScore = (clickCount: number) => {
-//     if (clickCount < bestScore || !bestScore) {
-//       setBestScore(clickCount);
-//     }
-//   };
-
-//   return {
-//     bestScore,
-//     updateBestScore,
-//   };
-// };
-
-// export default useGameMetrics;
-
 import { useState, useEffect } from "react";
 
 const useGameMetrics = () => {
